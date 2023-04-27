ad70d47d513338c57eda23dfbfe6374eed839afc
zs0608 <zss@princeton.edu>
2023-04-27 11:16:03 -0400

    edited validate

--- prover/validate.sh
+++ prover/validate.sh
@@ -8,11 +8,8 @@
 source /n/fs/484-nlproofs/miniconda3/bin/activate
 source activate nlproofs
 
-# Validate the stepwise prover without verifier-guided search on Task 2 of EntailmentBank.
-# python main.py validate --config cli_task2_stepwise_t5-large.yaml --ckpt_path /n/fs/484-nlproofs/weights/task2_prover.ckpt
-
 # Validate NLProofS (stepwise prover + verifier-guided search).
-python main.py validate --config cli_task2_stepwise_t5-large.yaml --ckpt_path /n/fs/484-nlproofs/weights/task2_prover.ckpt --model.verifier_weight 0.5 --model.verifier_ckpt /n/fs/484-nlproofs/weights/task2_verifier.ckpt --model.proof_search true
+# python main.py validate --config cli_task2_stepwise_t5-large.yaml --ckpt_path /n/fs/484-nlproofs/weights/task2_prover.ckpt --model.verifier_weight 0.5 --model.verifier_ckpt /n/fs/484-nlproofs/weights/task2_verifier.ckpt --model.proof_search true
 
-# Validate NLProofS w/o prover score.
-# python main.py validate --config cli_task2_stepwise_t5-large.yaml --ckpt_path /n/fs/484-nlproofs/weights/task2_prover.ckpt --model.verifier_weight 1.0 --model.verifier_ckpt /n/fs/484-nlproofs/weights/task2_verifier.ckpt --model.proof_search true
+# Validate NLProofs (oracle prover + verifier-guided search).
+python main.py validate --config cli_task2_stepwise_t5-large.yaml --ckpt_path /n/fs/484-nlproofs/weights/task2_prover.ckpt --model.verifier_weight 0.5 --model.verifier_ckpt /n/fs/484-nlproofs/weights/task2_verifier.ckpt --model.proof_search true --model.oracle_prover true
