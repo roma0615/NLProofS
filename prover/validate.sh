01a349f425a0a7e8ba3b3b4d4d0e726372705842
zs0608 <zss@princeton.edu>
2023-05-01 17:05:20 -0400

    cleanup

--- prover/validate.sh
+++ prover/validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
-#SBATCH --mem=16G
+#SBATCH --mem=32G
 #SBATCH --mail-user=zs0608@cs.princeton.edu
 #SBATCH --mail-type=ALL
 #SBATCH --output=slurm/%j.sh
@@ -8,8 +8,14 @@
 source /n/fs/484-nlproofs/miniconda3/bin/activate
 source activate nlproofs
 
+# Train a single-shot prover on Task 2 of EntailmentBank.
+python main.py fit --config cli_task2_stepwise_t5-small.yaml 
+
 # Validate NLProofS (stepwise prover + verifier-guided search).
 # python main.py validate --config cli_task2_stepwise_t5-large.yaml --ckpt_path /n/fs/484-nlproofs/weights/task2_prover.ckpt --model.verifier_weight 0.5 --model.verifier_ckpt /n/fs/484-nlproofs/weights/task2_verifier.ckpt --model.proof_search true
 
 # Validate NLProofs (oracle prover + verifier-guided search).
-python main.py validate --config cli_task2_stepwise_t5-large.yaml --ckpt_path /n/fs/484-nlproofs/weights/task2_prover.ckpt --model.verifier_weight 0.5 --model.verifier_ckpt /n/fs/484-nlproofs/weights/task2_verifier.ckpt --model.proof_search true --model.oracle_prover true
+# python main.py validate --config cli_task2_stepwise_t5-large.yaml --ckpt_path /n/fs/484-nlproofs/weights/task2_prover.ckpt --model.verifier_weight 0.5 --model.verifier_ckpt /n/fs/484-nlproofs/weights/task2_verifier.ckpt --model.proof_search true --model.oracle_prover true
+
+# Validate NLProofS (gpt prover + verifier-guided search).
+python main.py validate --config cli_task2_stepwise_t5-large.yaml --ckpt_path /n/fs/484-nlproofs/weights/task2_prover.ckpt --model.verifier_weight 0.5 --model.verifier_ckpt /n/fs/484-nlproofs/weights/task2_verifier.ckpt --model.proof_search true --model.gpt_prover true
