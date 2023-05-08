4941b5c590e46a544cfad37d17c70d9785bf2910
Roma Bhattacharjee <rb4785@rinse.cs.princeton.edu>
2023-05-08 16:41:42 -0400

    change config files for test and add slurm jobs

--- slurm/stepwise_contriever.sh
+++ slurm/stepwise_contriever.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem=32G
 #SBATCH --mail-user=rb4785@princeton.edu
 #SBATCH --mail-type=ALL
-#SBATCH --job-name=stepwise_contriever
+#SBATCH --job-name=tstepwise_contriever
 #SBATCH --output=%x_%j.log
 
 source /n/fs/484-nlproofs/miniconda3/bin/activate
@@ -12,4 +12,4 @@ cd ../prover
 
 # run stepwise model trained on task2, on task3 WITH DRAGON AS RETRIEVER
 # python main.py test --config cli_task3_stepwise_simcse_t5-large.yaml --ckpt_path ../../../weights/task2_prover.ckpt --model.verifier_ckpt ../../../weights/task2_verifier.ckpt --model.proof_search true
-python main.py validate --config cli_task3_stepwise_contriever_t5-large.yaml --ckpt_path ../../../weights/task2_prover.ckpt --model.verifier_ckpt ../../../weights/task2_verifier.ckpt --model.proof_search true
+python main.py test --config cli_task3_stepwise_contriever_t5-large.yaml --ckpt_path ../../../weights/task2_prover.ckpt --model.verifier_ckpt ../../../weights/task2_verifier.ckpt --model.proof_search true
